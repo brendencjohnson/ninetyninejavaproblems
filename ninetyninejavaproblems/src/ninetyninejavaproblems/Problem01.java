701e1bec7e2996c1c31d0e7da2a8ad38d37a2271
brenden <brendencjohnson@gmail.com>
2014-03-12 22:38:27 -0500

    complete Problem01

--- ninetyninejavaproblems/src/ninetyninejavaproblems/Problem01.java
+++ ninetyninejavaproblems/src/ninetyninejavaproblems/Problem01.java
@@ -3,9 +3,35 @@ package ninetyninejavaproblems;
 import java.util.List;
 
 public class Problem01 {
-	
-	public String getLastElement(List<String> list) {
-		return null;
+
+	public String getLastElementDirect(List<String> list) {
+		String result = null;
+		if (null != list && list.size() > 0) {
+			result = list.get(list.size() - 1);
+		}
+		return result;
+	}
+
+	public String getLastElementIterative(List<String> list) {
+		String result = null;
+		if (null != list && list.size() > 0) {
+			for (String element : list) {
+				result = element;
+			}
+		}
+		return result;
+	}
+
+	public String getLastElementDeclarative(List<String> list) {
+		if (null == list || list.size() == 0) {
+			return null;
+		}
+
+		if (list.size() > 1) {
+			return getLastElementDeclarative(list.subList(1, list.size()));
+		} else {
+			return list.get(0);
+		}
 	}
 
 }
